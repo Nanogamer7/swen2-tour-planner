e0a8f810ac0d93adda6820fd09f36bea0fa63cbf
Emile Durkheim <emile-durkheim@github.com>
2024-03-18 22:49:38 +0100

    added id's to the tour add/delete buttons

--- frontend/src/main/java/org/example/frontend/components/TourMenu/TourMenuController.java
+++ frontend/src/main/java/org/example/frontend/components/TourMenu/TourMenuController.java
@@ -2,6 +2,7 @@ package org.example.frontend.components.TourMenu;
 
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import org.example.frontend.EventHandler;
 import org.example.frontend.data.TourRepository;
@@ -9,6 +10,9 @@ import org.example.frontend.data.models.Tour;
 
 public class TourMenuController {
     private final TourMenuViewModel viewModel = new TourMenuViewModel();
+
+    public Button btnAddTour;
+    public Button btnDeleteTour;
 
     public ListView<String> lvTourNames;
 
