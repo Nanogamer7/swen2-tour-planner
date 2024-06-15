430530fdd492ee88f36a0005cae446dc76a59a0c
Emile Durkheim <emile-durkheim@github.com>
2024-06-15 15:10:05 +0200

    implemented client-side search

--- frontend/src/main/java/org/example/frontend/components/TourList/TourListController.java
+++ frontend/src/main/java/org/example/frontend/components/TourList/TourListController.java
@@ -3,6 +3,7 @@ package org.example.frontend.components.TourList;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import org.example.frontend.EventHandler;
 import org.example.frontend.base.TourUpdateListener;
 import org.example.frontend.data.TourRepository;
@@ -12,6 +13,7 @@ public class TourListController implements TourUpdateListener {
     private final TourListViewModel viewModel = new TourListViewModel();
 
     public ListView<String> lvTourNames;
+    public TextField txtSearchField;
 
 
     @FXML
@@ -46,6 +48,26 @@ public class TourListController implements TourUpdateListener {
 
     public void refreshToursList(){
         viewModel.setTours(TourRepository.getInstance().fetchTours());
+        lvTourNames.setItems(viewModel.getTourListViewNames());
+    }
+
+    public void onSearch(){
+        var searchQuery = txtSearchField.getText().toLowerCase();
+        var allTours = TourRepository.getInstance().fetchTours();
+
+        if(searchQuery.isEmpty()){
+            viewModel.setTours(allTours);
+        } else {
+            viewModel.setTours(allTours.stream().filter(
+                    (Tour tour) ->
+                            tour.name().toLowerCase().contains(searchQuery) ||
+                            tour.description().toLowerCase().contains(searchQuery) ||
+                            String.valueOf(tour.distance()).toLowerCase().contains(searchQuery) ||
+                            String.valueOf(tour.estimated_time()).toLowerCase().contains(searchQuery) ||
+                            tour.type().name().toLowerCase().contains(searchQuery)
+            ).toList());
+        }
+
         lvTourNames.setItems(viewModel.getTourListViewNames());
     }
 }
