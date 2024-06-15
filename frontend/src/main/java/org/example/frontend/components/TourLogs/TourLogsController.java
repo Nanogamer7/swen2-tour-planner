7f54f1d0e0d5fd38d1de0a7859227d0f774b0d21
Emile Durkheim <emile-durkheim@github.com>
2024-06-15 17:47:43 +0200

    kinda implemented tour log search??

--- frontend/src/main/java/org/example/frontend/components/TourLogs/TourLogsController.java
+++ frontend/src/main/java/org/example/frontend/components/TourLogs/TourLogsController.java
@@ -1,19 +1,27 @@
 package org.example.frontend.components.TourLogs;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.example.frontend.EventHandler;
 import org.example.frontend.base.TourUpdateListener;
 import org.example.frontend.data.TourRepository;
 import org.example.frontend.data.models.Tour;
 
+import java.util.List;
+
 public class TourLogsController implements TourUpdateListener {
     private final TourLogsViewModel viewModel = new TourLogsViewModel();
+    private List<TourLogsViewModel.FormattedTourLog> allTourLogs;
 
     public Button btnAddTourLog;
+
+    public TextField txtSearchField;
 
     public TableView tblLogs;
     public TableColumn colDate;
@@ -49,5 +57,26 @@ public class TourLogsController implements TourUpdateListener {
         viewModel.updateTourLogs(tourLogs);
 
         tblLogs.setItems(viewModel.formattedTourLogs);
+    }
+
+    public void onSearch(){
+        var searchQuery = txtSearchField.getText().toLowerCase();
+
+        ObservableList<TourLogsViewModel.FormattedTourLog> foundTourLogs;
+        if(searchQuery.isEmpty()){
+            foundTourLogs = viewModel.formattedTourLogs;
+        } else {
+            foundTourLogs = FXCollections.observableArrayList(viewModel.formattedTourLogs.stream().filter(
+                    (TourLogsViewModel.FormattedTourLog tourLog) ->
+                            tourLog.getDate().toLowerCase().contains(searchQuery) ||
+                            tourLog.getFormattedDuration().toLowerCase().contains(searchQuery) ||
+                            tourLog.getComment().toLowerCase().contains(searchQuery) ||
+                            String.valueOf(tourLog.getDifficulty()).toLowerCase().contains(searchQuery) ||
+                            String.valueOf(tourLog.getDistance()).toLowerCase().contains(searchQuery) ||
+                            String.valueOf(tourLog.getRating()).toLowerCase().contains(searchQuery)
+            ).toList());
+        }
+
+        tblLogs.setItems(foundTourLogs);
     }
 }
